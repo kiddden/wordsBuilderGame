55340a9f15feece7cadb2a96976aa93da8c04412
Kazzer <kazzer.nd@gmail.com>
2022-07-11 21:05:01 +0300

    Initial Commit

---
+++ words builder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  words builder
+//
+//  Created by Eugene Ned on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
